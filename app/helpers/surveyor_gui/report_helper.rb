7660523a28c9daa065bdbad75f9d305efea9e09d
Kevin Jay <kjayma@gmail.com>
2014-08-07 11:56:07 -0400

    Fix datetime labels on histogram
    Factor out histogram calcs into HistogramArray.
    Remove report formatting from helper and put in new
    PORO facade class, ReportFormatter.
    	modified:   app/controllers/surveyor_gui/reports_controller.rb
    	new file:   app/facades/report_formatter.rb
    	modified:   app/helpers/surveyor_gui/report_helper.rb
    	modified:   app/views/surveyor_gui/reports/_report_data.html.haml
    	modified:   app/views/surveyor_gui/reports/_show_report.html.haml
    	modified:   lib/surveyor_gui/engine.rb

--- app/helpers/surveyor_gui/report_helper.rb
+++ app/helpers/surveyor_gui/report_helper.rb
@@ -30,23 +30,4 @@ module SurveyorGui::ReportHelper
     (responses.where(:question_id => q.id).where('integer_value > ?',0).collect(&:integer_value).average * 2).round
   end
 
-  def stats(q, stat_function)
-    stat = calculate_stats(q, stat_function)
-    format_stats(q, stat)
-  end
-
-  def calculate_stats(q, stat_function)
-    arr =  @responses.where(:question_id => q.id).map{|r| r.response_value.to_f}
-    STAT_FUNCTIONS[stat_function].call(arr)
-  end
-  
-  def format_stats(q, stat)
-    if q.question_type_id == :number
-      STAT_FORMATS[q.question_type_id] % stat 
-    else
-      stat = Time.zone.at(stat)     
-      stat.strftime(STAT_FORMATS[q.question_type_id])
-    end
-  end
-
 end
